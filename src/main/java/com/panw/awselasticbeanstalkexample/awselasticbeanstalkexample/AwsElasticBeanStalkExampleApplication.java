c8fdd381302e24e90cefbae3705b8f6bfbdfd24c
Vivek Rangayan <vrangayan@sjcmacj13khtd8.paloaltonetworks.local>
2019-11-04 23:18:57 -0800

    added buildspec.yml

---
+++ src/main/java/com/panw/awselasticbeanstalkexample/awselasticbeanstalkexample/AwsElasticBeanStalkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.panw.awselasticbeanstalkexample.awselasticbeanstalkexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanStalkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanStalkExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/panw/awselasticbeanstalkexample/awselasticbeanstalkexample/AwsElasticBeanStalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panw.awselasticbeanstalkexample.awselasticbeanstalkexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanStalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
